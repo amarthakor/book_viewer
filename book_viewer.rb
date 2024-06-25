b20efaa3df16fe6beabd3ea65a3421dc2d253ec2
Amar Thakor <amarthakor97@gmail.com>
2024-06-24 23:12:10 -0700

    Added search functionality to application

--- book_viewer.rb
+++ book_viewer.rb
@@ -12,6 +12,10 @@ helpers do
       "<p>#{paragraph}</p>"
     end.join
   end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
 end
 
 get "/" do
@@ -29,6 +33,53 @@ get "/chapters/:number" do
   @chapter = File.read "data/chp#{number}.txt"
 
   erb :chapter, layout: :layout
+end
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+get "/search" do
+
+  if params[:query]
+    # each result in this array will be:
+    # [ chapter name, chapter index, paragraph index ]
+    @results = @contents.each_with_index.each_with_object([]) do |(chapter, index), results|
+      text = File.read("data/chp#{index + 1}.txt")
+      paragraphs = text.split("\n\n")
+      paragraphs.each_with_index do |paragraph, paragraph_index|
+        if paragraph.include?(params[:query])
+          results << [chapter, index, paragraph, paragraph_index]
+        end
+      end
+    end
+  end
+
+  erb :search
 end
 
 not_found do
