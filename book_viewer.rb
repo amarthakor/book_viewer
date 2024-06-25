0709479f16d184e2b6218e54df1b8fb5eb2ee32d
Amar Thakor <amarthakor97@gmail.com>
2024-06-24 21:56:19 -0700

    Added features such as before block and helpers block to application

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,17 +2,31 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines "data/toc.txt"
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines "data/toc.txt"
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines "data/toc.txt"
-  @chapter = File.read "data/chp1.txt"
+get "/chapters/:number" do
+  number = params[:number].to_i
 
-  erb :chapter
+  @title = "Chapter #{number}"
+  @chapter = File.read "data/chp#{number}.txt"
+
+  @chapter_name = "Chapter #{number}: " + @contents[number - 1]
+
+  erb :chapter, layout: :layout
 end
